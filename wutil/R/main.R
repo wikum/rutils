a76143d533d8e48655fd0dd7883b36cfd548f197
Wikum <wikumdina@gmail.com>
2017-06-22 14:42:24 -0400

    Adding package

---
+++ wutil/R/main.R
@@ -0,0 +1,16 @@
+
+GetPredictionStats <- function(predictions, truth, classes=NULL, decision_values=NULL){
+  
+  getPredictionStats(predictions, truth, classes, decision_values)
+
+}
+
+
+
+
+
+
+
+
+
+
